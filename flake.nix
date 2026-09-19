bfceecbad5d9545ed2538b8c9690b9061c04213b
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-09-19 11:25:26 +0200

    fix(nix): migrate plinth input to github

--- flake.nix
+++ flake.nix
@@ -13,9 +13,8 @@
       flake = false;
     };
     plinth = {
-      url = "git+https://codeberg.org/caniko/plinth.git?ref=refs/heads/trunk";
+      url = "git+https://github.com/caniko/plinth.git?ref=refs/heads/trunk";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
